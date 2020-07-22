af000211103c52ffd72e10286db4a5143797c7e6
Atharva Ashish Chitnavis <47708712+ath98@users.noreply.github.com>
2020-07-22 23:36:01 +0530

    First face detection version

---
+++ controll.py
@@ -0,0 +1,12 @@
+
+from create_data import add_data
+from face_recognize import recognize
+print("Press 1-To insert new face in database\n 2- To recognise faces")
+user=input("Enter here")
+
+if(user=='a'):
+    print("Enter Name of user")
+    name=input("Enter name here")
+    add_data(name)
+if(user=='b'):
+    recognize()
